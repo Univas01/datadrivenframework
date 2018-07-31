a955f619ddcc6c3e929addb7e3a1cd1029c0b169
Ola Ajala <ola.ajala1@outlook.com>
2018-07-31 12:14:30 +0100

    updating project

---
+++ src/test/java/datadriven/DateColFromExcel.java
@@ -0,0 +1,100 @@
+package datadriven;
+
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+public class ExcelDataDrivenLoop {
+
+    public static WebDriver driver;
+    public static Properties prop;
+    public Workbook workbook;
+    public Sheet sh1;
+
+    @BeforeMethod
+    public static void setUp(){
+        try{
+           prop  = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/resources/config.properties");
+            prop.load(ip);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void excelDataTest() throws ParseException {
+        try{
+            File dataFile = new File(System.getProperty("user.dir")+"/UnitedAirlineData.xlsx");
+            FileInputStream inputStream = new FileInputStream(dataFile);
+            BufferedInputStream bufferStream = new BufferedInputStream(inputStream);
+            workbook = WorkbookFactory.create(bufferStream);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        } catch (InvalidFormatException e){
+            e.printStackTrace();
+        }
+
+        // Create an object for the sheet
+        sh1 = workbook.getSheet("sheet1");
+
+        // Count number of rows in excel sheet
+        int lastRow = sh1.getLastRowNum();
+        System.out.println("Total number of Rows is :"+lastRow);
+
+        // for loop to automate all the test scenarios
+        for(int i = 1; i <= lastRow; i++){
+
+            // Cell variable
+            int j = 0;
+
+            // Create variables
+            String from = sh1.getRow(i).getCell(j).getStringCellValue();
+            String to = sh1.getRow(i).getCell(++j).getStringCellValue();
+            String tripType = sh1.getRow(i).getCell(++j).getStringCellValue();
+
+            double departDateNumber = (double) sh1.getRow(i).getCell(++j).getNumericCellValue();
+            Date departDate = DateUtil.getJavaDate(departDateNumber);
+            SimpleDateFormat formatDepartDate = new SimpleDateFormat("dd/MM/yyyy");
+
+            double returnDateNumber = (double) sh1.getRow(i).getCell(++j).getNumericCellValue();
+            Date returnDate = DateUtil.getJavaDate(returnDateNumber);
+            SimpleDateFormat formatReturnDate = new SimpleDateFormat("dd/MM/yyyy");
+
+            int noOfPassengers = (int)sh1.getRow(i).getCell(++j).getNumericCellValue();
+            String flightclass = sh1.getRow(i).getCell(++j).getStringCellValue();
+
+            System.out.println("from : "+ from);
+            System.out.println("to : "+ to);
+            System.out.println("triptype : "+tripType);
+            System.out.println("departDate :"+formatDepartDate.format(departDate));
+            System.out.println("returnDate :"+formatReturnDate.format(returnDate));
+            System.out.println("noOfPassengers :"+noOfPassengers);
+            System.out.println("flightclass : "+flightclass);
+        }
+    }
+
+
+
+    @AfterMethod (enabled = false)
+    public void tearDown(){
+        driver.quit();
+    }
+}
--- src/test/java/datadriven/ExcelDataDrivenLoop.java
+++ src/test/java/datadriven/ExcelDataDrivenLoop.java
@@ -1,100 +1,167 @@
 package datadriven;
 
-
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
-public class ExcelDataDrivenLoop {
+public class ExcelDataDrivenLoop_1 {
 
     public static WebDriver driver;
     public static Properties prop;
     public Workbook workbook;
-    public Sheet sh1;
-
-    @BeforeMethod
-    public static void setUp(){
-        try{
-           prop  = new Properties();
-            FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/resources/config.properties");
-            prop.load(ip);
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
+    public Sheet sh2;
 
     @Test
-    public void excelDataTest() throws ParseException {
-        try{
-            File dataFile = new File(System.getProperty("user.dir")+"/UnitedAirlineData.xlsx");
+    public void excelDataTest() {
+        try {
+            File dataFile = new File(System.getProperty("user.dir") + "/UnitedAirlineData.xlsx");
             FileInputStream inputStream = new FileInputStream(dataFile);
             BufferedInputStream bufferStream = new BufferedInputStream(inputStream);
             workbook = WorkbookFactory.create(bufferStream);
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        } catch (InvalidFormatException e){
+        } catch (InvalidFormatException e) {
             e.printStackTrace();
         }
 
         // Create an object for the sheet
-        sh1 = workbook.getSheet("sheet1");
+        sh2 = workbook.getSheet("sheet2");
 
         // Count number of rows in excel sheet
-        int lastRow = sh1.getLastRowNum();
-        System.out.println("Total number of Rows is :"+lastRow);
+        int lastRow = sh2.getLastRowNum();
+        System.out.println("Total number of Rows is :" + lastRow);
 
-        // for loop to automate all the test scenarios
         for(int i = 1; i <= lastRow; i++){
-
-            // Cell variable
             int j = 0;
 
-            // Create variables
-            String from = sh1.getRow(i).getCell(j).getStringCellValue();
-            String to = sh1.getRow(i).getCell(++j).getStringCellValue();
-            String tripType = sh1.getRow(i).getCell(++j).getStringCellValue();
+            System.out.println("======================================");
+            String from = sh2.getRow(i).getCell(j).getStringCellValue();
+            String to = sh2.getRow(i).getCell(++j).getStringCellValue();
+            String tripType = sh2.getRow(i).getCell(++j).getStringCellValue();
+            String departDate = sh2.getRow(i).getCell(++j).getStringCellValue();
+            String returnDate = sh2.getRow(i).getCell(++j).getStringCellValue();
+            int noOfPassenger = (int) sh2.getRow(i).getCell(++j).getNumericCellValue();
+            String flightClass = sh2.getRow(i).getCell(++j).getStringCellValue();
 
-            double departDateNumber = (double) sh1.getRow(i).getCell(++j).getNumericCellValue();
-            Date departDate = DateUtil.getJavaDate(departDateNumber);
-            SimpleDateFormat formatDepartDate = new SimpleDateFormat("dd/MM/yyyy");
+            try{
+                prop  = new Properties();
+                FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/resources/config.properties");
+                prop.load(ip);
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
 
-            double returnDateNumber = (double) sh1.getRow(i).getCell(++j).getNumericCellValue();
-            Date returnDate = DateUtil.getJavaDate(returnDateNumber);
-            SimpleDateFormat formatReturnDate = new SimpleDateFormat("dd/MM/yyyy");
+            String browserName = prop.getProperty("browser");
 
-            int noOfPassengers = (int)sh1.getRow(i).getCell(++j).getNumericCellValue();
-            String flightclass = sh1.getRow(i).getCell(++j).getStringCellValue();
+            if(browserName.equalsIgnoreCase("chrome")){
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/browsers/chromedriver");
+                driver = new ChromeDriver();
+            } else if(browserName.equalsIgnoreCase("firefox")){
+                System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/browsers/geckodriver");
+                driver = new FirefoxDriver();
+            } else if(browserName.equalsIgnoreCase("safari")){
+                driver = new SafariDriver();
+            }
 
-            System.out.println("from : "+ from);
-            System.out.println("to : "+ to);
-            System.out.println("triptype : "+tripType);
-            System.out.println("departDate :"+formatDepartDate.format(departDate));
-            System.out.println("returnDate :"+formatReturnDate.format(returnDate));
-            System.out.println("noOfPassengers :"+noOfPassengers);
-            System.out.println("flightclass : "+flightclass);
+            driver.manage().window().maximize();
+            driver.manage().deleteAllCookies();
+            driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+            driver.get(prop.getProperty("url"));
+            driver.findElement(By.cssSelector(".language-region-change")).click();
+
+            System.out.println("from : "+from);
+            System.out.println("to : "+to);
+            System.out.println("tripType : "+tripType);
+            System.out.println("departDate : "+departDate);
+            System.out.println("returnDate : "+returnDate);
+            System.out.println("noOfPassenger : "+noOfPassenger);
+            System.out.println("flightClass : "+flightClass);
+
+            WebElement departFrom_ftf = driver.findElement(By.id("Origin"));
+            departFrom_ftf.clear();
+            departFrom_ftf.sendKeys(from);
+
+            WebElement destination_ftt = driver.findElement(By.id("Destination"));
+            destination_ftt.clear();
+            destination_ftt.sendKeys(to);
+
+            WebElement tripType_radioBtn = driver.findElement(By.xpath("//input[@id=\'"+tripType+"\']"));
+            tripType_radioBtn.click();
+
+            WebElement departDateField = driver.findElement(By.xpath("//input[@id='DepartDate']"));
+            String departDateValue = departDate;
+            departDateField.clear();
+            selectDateByJS(departDateField,departDateValue);
+
+            WebElement returnDateField = driver.findElement(By.xpath("//input[@id='ReturnDate']"));
+            String returnDateValue = returnDate;
+            returnDateField.clear();
+            selectDateByJS(returnDateField,returnDateValue);
+
+            WebElement noOfPassengerDDM = driver.findElement(By.xpath("//a[@id='travelers-selector']/child::*/child::span[2]"));
+            noOfPassengerDDM.click();
+            WebElement selectNoOfPass = driver.findElement(By.xpath("//input[@name='NumOfAdults']"));
+            selectNoOfPass.sendKeys(Keys.BACK_SPACE);
+            sendKeys(driver, selectNoOfPass, 10, String.valueOf(noOfPassenger));
+            driver.findElement(By.xpath("//div[@id='travelers-select']/descendant::button")).click();
+
+            WebElement selectCabinType = driver.findElement(By.id("cabinType"));
+            Select selectCabinTypes = new Select(selectCabinType);
+            selectCabinTypes.selectByVisibleText(flightClass);
+
+            WebElement submitFlightBookingBtn = driver.findElement(By.xpath("//button[@id='flightBookingSubmit']"));
+            clickOnJSObject(submitFlightBookingBtn);
+
+            String title = driver.getTitle();
+            Assert.assertEquals(title,"Flight Search Results | United Airlines");
+            System.out.println(title);
+
+            WebElement departure = driver.findElement(By.xpath("//div[@class='lof-origin2']"));
+            String departureText = departure.getText();
+            System.out.println(departureText);
+            boolean flag = departure.isDisplayed();
+            Assert.assertTrue(flag);
+
+            driver.quit();
         }
     }
 
+    public static void selectDateByJS(WebElement element, String dateValue) {
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].setAttribute('value','"+dateValue+"');", element);
+    }
 
+    public static void clickMethod(WebDriver driver, long timeout, WebElement element) {
+        WebDriverWait one = new WebDriverWait(driver, timeout);
+        one.until(ExpectedConditions.elementToBeClickable(element));
+    }
 
-    @AfterMethod (enabled = false)
-    public void tearDown(){
-        driver.quit();
+    public static void sendKeys(WebDriver driver, WebElement element, int timeout, String value) {
+        new WebDriverWait(driver, timeout).until(ExpectedConditions.visibilityOf(element));
+        element.sendKeys(value);
+    }
+
+    public static void clickOnJSObject(WebElement element){
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].click();", element);
     }
 }
