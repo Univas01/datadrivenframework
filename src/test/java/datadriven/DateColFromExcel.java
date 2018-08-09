7d90aaf2a932b3d8a18af1d7279ed7dc4a9cbebf
Ola Ajala <ola.ajala@worldfirst.com>
2018-08-09 17:33:51 +0100

    Modification

--- src/test/java/datadriven/DateColFromExcel.java
+++ src/test/java/datadriven/DateColFromExcel.java
@@ -6,7 +6,9 @@ import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -26,6 +28,7 @@ public class DateColFromExcel {
 
     @BeforeMethod
     public static void setUp(){
+
         try{
            prop  = new Properties();
             FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/resources/config.properties");
